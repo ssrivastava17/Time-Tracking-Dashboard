7c5e167a4d73ecf2a4dc785f8a3f82c689d8fb30
Stephanie Srivastava <seichman2011@gmail.com>
2022-06-22 14:11:39 -0400

    new version

--- src/App.svelte
+++ src/App.svelte
@@ -1,88 +1,140 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
   import Activity from './lib/Activity.svelte';
   import timeData from '../data.json';
-  import {writable} from 'svelte/store';
+  import headshot from './assets/image-jeremy.png';
 
-  
+ 
+  let myTimeframe = "Weekly";
+  let colors = ["hsl(15, 100%, 70%)", "hsl(195, 74%, 62%)", "hsl(348, 100%, 68%)", "hsl(145, 58%, 55%)", "hsl(264, 64%, 52%)", "hsl(43, 84%, 65%)"]
 
-  let myTimeframe ="Weekly";
-  const whatever = writable(myTimeframe);
 
   function updateDaily(){
-    $whatever = "Daily";
+    myTimeframe = "Daily";
     console.log("Daily");
   }
 
   function updateWeekly(){
-    $whatever = "Weekly";
+    myTimeframe = "Weekly";
+    // $whatever = "Weekly";
     console.log("Weekly");
   }
 
   function updateMonthly(){
-    $whatever = "Monthly";
+    myTimeframe = "Monthly";
+    // $whatever = "Monthly";
     console.log("Monthly");
   }
 
 </script>
 
-<main>
-  <button on:click={updateDaily}>Daily</button>
-  <button on:click={updateWeekly}>Weekly</button>
-  <button on:click={updateMonthly}>Monthly</button>
+<body style="margin: 0;">
+  <div class="wrapper">
+    <div class="infoCard">
+      <div class="info">
+          <img src={headshot} alt="headshot of Jeremy">
+          <p>Report for</p>
+          <p class="name">Jeremy</p>
+          <p class="name">Robson</p>
+      </div>
+      
+      <button on:click={updateDaily}>Daily</button>
+      <button on:click={updateWeekly}>Weekly</button>
+      <button on:click={updateMonthly}>Monthly</button>
 
-  <!-- <Activity title="Work" thisWeek={timeData[0].timeframes.daily.current} lastWeek=5/> -->
-  {#each timeData as myTime}
-    <Activity time={myTime} timeframe={whatever} />
-  {/each}
-
-  <!-- <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p> -->
-</main>
+    </div>
+    {#each timeData as myTime, i}
+      <Activity time={myTime} timeframe={myTimeframe} color={colors[i]} image={i}/>
+     
+    {/each}
+  </div>
+  <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="https://github.com/ssrivastava17">Stephanie Srivastava</a>.
+  </div>
+</body>
 
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500&display=swap');
+  
   :root {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
       Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 
-  main {
+  button{
+    color: white;
+    border: none;
+    background-color: transparent;
+    font-family: 'Rubik', sans-serif;
+    padding-left: 10%;
+    padding-top: 5%;
+    display: block;
+  }
+
+  .wrapper{
+    display: grid;
+    width: 825px;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: 190px 180px;
+    margin: auto;
+    padding: 100px 10px;
+    column-gap: 20px;
+    row-gap: 20px;
+  }
+
+  .infoCard{
+    background-color: hsl(235, 46%, 20%);
+    color: white;
+    font-family: 'Rubik', sans-serif;
+    width: 200px;
+    border-radius: 25px;
+    grid-column-start: 1;
+    grid-row-start: 1;
+    grid-row-end: 3;
+    text-align: left;
+  }
+
+  .info{
+    background-color: hsl(246, 80%, 60%);
+    border-radius: 25px;
+    padding: 10%;
+  }
+
+  body{
     text-align: center;
-    padding: 1em;
-    margin: 0 auto;
+    align-items: center;
+    background-color: hsl(226, 43%, 10%);
+    overflow: hidden;
+    width: 100vw;
+    height: 100vh;
+  }
+
+  body::before{
+    height: 100%;
+    width: 100vw;
+    left: 50%;
+    top:0;
+    transform: translateX(-50%); 
+    background: yellowgreen;
   }
 
   img {
-    height: 16rem;
-    width: 16rem;
+    height: 5rem;
+    width: 5rem;
+    border: 5px solid white;
+    border-radius: 50%;
+  
   }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
+  .attribution{
+    color: white;
   }
-
   p {
     max-width: 14rem;
     margin: 1rem auto;
     line-height: 1.35;
   }
 
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
 
-    p {
-      max-width: none;
-    }
-  }
+
 </style>
--- src/lib/Activity.svelte
+++ src/lib/Activity.svelte
@@ -1,9 +1,20 @@
 <script>
     export let time; // indicates that Activity takes text as prop
     export let timeframe;
+    export let color;
+    export let image;
+     import work from '../assets/icon-work.svg';
+     import play from '../assets/icon-play.svg';
+     import study from '../assets/icon-study.svg';
+     import exercise from '../assets/icon-exercise.svg';
+     import social from '../assets/icon-social.svg';
+     import self from '../assets/icon-self-care.svg';
+    import ellipsis from '../assets/icon-ellipsis.svg';
+    let images= [work, play, study, exercise, social, self];
     let thisWeek;
     let lastWeek;
 
+    $: {
    if(timeframe == "Daily"){
         thisWeek = time.timeframes.daily.current;
         lastWeek = time.timeframes.daily.previous;
@@ -14,15 +25,66 @@
         thisWeek = time.timeframes.monthly.current;
         lastWeek = time.timeframes.monthly.previous;
    }
+}
 
 </script>
 
-<div class="background">
+<!-- background-color:{color}; -->
+
+<div class="background" style="background-color:{color}; background-image: url({images[image]});">
 
     <div class="card">
-        <p>{time.title} </p>
-        <p>{thisWeek}hrs </p>
-        <p>Last Week - {lastWeek}hrs </p>
+        <div class="title">{time.title} </div>
+        <div class="current">{thisWeek}hrs </div>
+        <div class="last">Last Week - {lastWeek}hrs </div>
     </div>
 
 </div>
+
+<style>
+     @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500&display=swap');
+     .card{
+          border: 1px black;
+          background-color: hsl(235, 46%, 20%);
+          text-align: left;
+          top: 50%;
+          left: 50%;
+          transform: translate(0%, 18%);
+          border-radius: 15px;
+          height: 80%;
+          padding: 5% 0 0 10%;
+          color: white;
+          /* visibility: hidden; */
+     }
+
+     .title{
+          font-size: 18px;
+          padding: 10px 0;
+          width: 100px;
+     }
+
+     .title::before{
+          content: url(ellipsis);
+          position: absolute;
+          right: 100px; 
+     }
+
+     .current{
+          font-family: 'Rubik', sans-serif;
+          font-size: 40px;
+          font-weight: 300;
+          padding: 15px 0 5px 0;
+     }
+
+     .last{
+          font-size: 12px;
+          color: hsl(236, 100%, 87%);
+     }
+
+     .background{
+          background-repeat: no-repeat;
+          border-radius: 15px;
+          background-position: 90% -3%;
+          background-size: 25%;
+     }
+</style>
